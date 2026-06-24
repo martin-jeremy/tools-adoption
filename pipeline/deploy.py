52aaf291dbf2a20fe26ac5f7d07041598cb44352
Jérémy MARTIN <jeremy.martin@ascentry.com>
2026-06-24 02:15:25 +0200

    Update: Switch Prefect image to Python 3.12, refactor main orchestrator flow and dependencies. Add: dbt transformation and dbt cleaning to the Prefect flow

--- pipeline/deploy.py
+++ pipeline/deploy.py
@@ -2,13 +2,20 @@ import sys
 from pathlib import Path
 sys.path.append(str(Path.cwd()))
 
+from prefect import flow
 from pipeline.flows._01_extract_and_load import ingestion_flow, default_repos
+from pipeline.flows._02_transforms import dbt_flow, dbt_clean
+
+@flow(name="main-orchestrator")
+def main_orchestrator():
+    ingestion_flow(default_repos)
+    dbt_flow()
+    dbt_clean()
 
 if __name__ == "__main__":
     # .serve() permet d'exposer le flux en tant que service
     # sans avoir besoin de reconstruire une image docker
-    ingestion_flow.serve(
-        name='ingestion-flow',
+    main_orchestrator.serve(
+        name='main-orchestrator',
         cron='0 0 * * *',
-        parameters={'repos': default_repos}
     )
